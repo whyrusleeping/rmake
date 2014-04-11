d9c41013adf9ce445d307c5b9c9666fd11de27e8
Travis <travis.lane.osx@gmail.com>
2014-04-11 15:16:29 -0700

    syncing message changes

--- rmake/messagetype.go
+++ rmake/messagetype.go
@@ -1,22 +1,44 @@
 package main
 
+type BuilderRequest struct {
+	//
+	Input []File
+	//
+	Command string
+	//
+	ResultAddress string
+	//
+	Session string
+}
+
 //A response that is sent back from the server
 //contains the result of a build
 type BuilderResult struct {
+	//
+	Results []File
+	//
 	Stdout  string
+	//
 	Error   string
-	Binary  *File
+	//
 	Success bool
+	//
 	Session string
 }
 
 //A build package, gets sent to the server to start a build
 type ManagerRequest struct {
-	Type    int
-	Files   []*File
-	Command string
-	Args    []string
-	Output  string
+	//
+	Jobs int
+	//
+	Arch string
+	//
+	OS   string
+}
+
+type ManagerResult struct {
+	//
+	Builders string[]
+	//
 	Session string
-	Vars    map[string]string
 }
