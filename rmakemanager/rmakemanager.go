c85737a1b350d066bc0655fbfc4eb59f6a83472c
Travis <travis.lane.osx@gmail.com>
2014-04-16 18:31:15 -0700

    Working on server side of builder subscription

--- rmakemanager/rmakemanager.go
+++ rmakemanager/rmakemanager.go
@@ -10,8 +10,48 @@ import (
 	"github.com/whyrusleeping/rmake/types"
 )
 
+type Manager struct {
+	getUuid chan int
+	putUuid chan int
+
+	list net.Listener
+}
+
+func NewManager(listname string) *Manager {
+
+	//Start the server socket
+	list, err := net.Listen("tcp", listname)
+	if err != nil {
+		panic(err)
+	}
+	m := new(Manager)
+	m.list = list
+	go m.UUIDGenerator()
+	return m
+}
+
+func (m *Manager) UUIDGenerator() {
+	var free []int
+	nextUuid := 0
+	maxUuid := 0
+	for {
+		select {
+		case m.getUuid <- nextUuid:
+			if len(free) > 0 {
+				nextUuid = free[0]
+				free = free[1:]
+			} else {
+				maxUuid++
+				nextUuid = maxUuid
+			}
+		case id := <-m.putUuid:
+			free = append(free, id)
+		}
+	}
+}
+
 // Allocate resources to the request
-func HandleManagerRequest(request *rmake.ManagerRequest) {
+func (m *Manager) HandleManagerRequest(request *rmake.ManagerRequest) {
 	// handle the request
 
 	// So do we want to keep a line open for the FinalBuildResult?
@@ -19,10 +59,15 @@ func HandleManagerRequest(request *rmake.ManagerRequest) {
 	// Questions for tomorrow.
 }
 
+//
+func (m *Manager) HandleBuilderAnnouncement(bldr *rmake.BuilderAnnouncement, con net.Conn) {
+	//con.RemoteAddr()
+}
+
 // goroutine to handle a new connection from a client.
 // Determines what resources are avaliable and what
 // resources the request requires.
-func HandleConnection(c net.Conn) {
+func (m *Manager) HandleConnection(c net.Conn) {
 	var gobint interface{}
 
 	unzip, err := gzip.NewReader(c)
@@ -44,11 +89,25 @@ func HandleConnection(c net.Conn) {
 		fmt.Printf("Manager Request: %d\n", gobtype)
 	case *rmake.BuilderAnnouncement:
 		fmt.Printf("Builder Announcement: %d\n", gobtype)
+		m.HandleBuilderAnnouncement(gobint.(*rmake.BuilderAnnouncement), c)
 	default:
 		fmt.Printf("Unknown Type.\n", gobtype)
 	}
 
 	return
+}
+
+func (m *Manager) Start() {
+	//Accept and handle new client connections
+	for {
+		con, err := m.list.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		//Handle clients in separate 'thread'
+		go m.HandleConnection(con)
+	}
 }
 
 // main
@@ -65,21 +124,6 @@ func main() {
 
 	fmt.Println("rmakemanager\n")
 
-	//Start the server socket
-	list, err := net.Listen("tcp", listname)
-	if err != nil {
-		panic(err)
-	}
-
-	//Accept and handle new client connections
-	for {
-		con, err := list.Accept()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		//Handle clients in separate 'thread'
-		go HandleConnection(con)
-	}
-
+	manager := NewManager(listname)
+	manager.Start()
 }
